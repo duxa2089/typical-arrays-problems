47f0e060a0856b048fce30f9e4513f3b87fa4086
duxa2089 <duxa2089@mail.ru>
2021-02-09 00:52:51 +0300

    Solve question

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let avg = 0;
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        avg += array[i];
+        count++;
+    }
+    return (avg / count);
+};
